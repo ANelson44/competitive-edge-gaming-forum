ac7341094ad6ef22ed305a9b53d392d4acfc3f0c
Joshua Eagan <JoshuaPEagan@outlook.com>
2024-01-18 20:36:29 -0500

    Created the models and connections to each other.

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'The title cannot be empty.'
+            }
+        }
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'The post must not be empty.'
+            }
+        }
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'User',
+            key: 'id',
+        }
+    }
+}, {
+    sequelize,
+    modelName: 'Post',
+    freezeTableName: true,
+    timestamps: true,
+});
+
+module.exports = Post;
